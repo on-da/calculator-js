1da9055f6587332796c57c31647312ff7452a2c9
on-da <53707132+on-da@users.noreply.github.com>
2019-09-08 14:24:50 +0900

    Delete calculator.js

--- calculator.js
+++
@@ -1,8 +0,0 @@
-
-function edit(e) {
-    let result = document.getElementById("result");
-    result.value = result.value + e.value;
-}
-function calc() {
-    result.value = new Function("return " + result.value)();
-}
