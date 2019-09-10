0fc0d11eaf61b5556ae8a889ad13b0771f62d304
on-da <revu5121@gmail.com>
2019-09-10 17:36:19 +0900

    デバック未　取りあえず一応完成？

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 //計算状況の各変数を用意　result=最終合計値　total=計算中の合計値 
 //key=入力値　mark=演算子 flag=直前の入力値の種類の判別用の変数　0:数値 1:演算子 2:=
 let result = 0;
+let editResult=0;
 let key = "";
 let mark = "";
 let flag = 0;
@@ -17,6 +18,7 @@ function recordNumber(e) {
     } else {
         flag = 0;
         result = 0;
+        editResult=0;
         key = "";
         mark = "";
         key += e;
@@ -32,20 +34,81 @@ function recordNumber(e) {
 //記号を入力した場合の関数calc()。＝とそれ以外で条件分岐
 function calc(e) {
     if (e === "=") {
-        flag=2;
+        if (mark === "+") {
+            flag = 2;
+            editResult = Number(result) + Number(key);
+            document.getElementById("result").value = editResult;
+        } else if (mark === "-") {
+            flag = 2;
+            editResult = Number(result) - Number(key);
+            document.getElementById("result").value = editResult;
+        } else if (mark === "*") {
+            flag = 2;
+            editResult = Number(result) * Number(key);
+            document.getElementById("result").value = editResult;
+        } else if (mark === "/") {
+            flag = 2;
+            editResult = Number(result) / Number(key);
+            document.getElementById("result").value = editResult;
+        }
+    } else if (e === "+") {
+        flag = 1;
         mark = e;
-
-        document.getElementById("result").value = result;
-    } else if(e==="+") {
-        flag=1;
-        mark = e;
-        let sumResult= Number(result)+Number(key);
+        let sumResult = Number(result) + Number(key);
         key = "";
-        result=sumResult;
-        document.getElementById("result").value=sumResult;
-
+        result = sumResult;
+        document.getElementById("result").value = result;
+    } else if (e === "-") {
+        if (result === 0) {
+            flag = 1;
+            mark = e;
+            result = Number(key)
+            key = "";
+            document.getElementById("result").value = result;
+        } else {
+            flag = 1;
+            mark = e;
+            let sumResult = Number(result) - Number(key);
+            key = "";
+            result = sumResult;
+            document.getElementById("result").value = result;
+        }
+    } else if (e === "*") {
+        if (result === 0) {
+            flag = 1;
+            mark = e;
+            result = Number(key)
+            key = "";
+            document.getElementById("result").value = result;
+        } else {
+            flag = 1;
+            mark = e;
+            let sumResult = Number(result) * Number(key);
+            key = "";
+            result = sumResult;
+            document.getElementById("result").value = sumResult;
+        }
+    } else if (e === "/") {
+        if (result === 0) {
+            flag = 1;
+            mark = e;
+            result = Number(key)
+            key = "";
+            document.getElementById("result").value = result;
+        } else {
+            flag = 1;
+            mark = e;
+            let sumResult = Number(result) / Number(key);
+            key = "";
+            result = sumResult;
+            document.getElementById("result").value = sumResult;
+        }
     }
+
+
+
 }
+
 
 //クリアキーを押したときのclearAll()関数
 function clearAll() {
